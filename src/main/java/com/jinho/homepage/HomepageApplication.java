780b84ee84ad3e0e04931ff1e3c03f5eea209a65
JinoRino <adg0609@naver.com>
2021-07-29 14:59:45 +0900

    war 파일 배포(tomcat 의존 추가)

--- src/main/java/com/jinho/homepage/HomepageApplication.java
+++ src/main/java/com/jinho/homepage/HomepageApplication.java
@@ -2,9 +2,10 @@ package com.jinho.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HomepageApplication {
+public class HomepageApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomepageApplication.class, args);
