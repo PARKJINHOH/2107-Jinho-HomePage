1b6d8bda770d357ca8009322d84c2f198ef4111d
JinoRino <adg0609@naver.com>
2021-08-12 15:39:03 +0900

    mainService 삭제로 인한 대응

--- src/main/java/com/jinho/homepage/controller/MainController.java
+++ src/main/java/com/jinho/homepage/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.jinho.homepage.controller;
 
-import com.jinho.homepage.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class MainController {
-
-    private final MainService mainService;
-
     @GetMapping("/")
     public String getMainPage() {
         return "/mainPage/mainPage";
