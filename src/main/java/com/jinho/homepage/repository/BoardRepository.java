4a22d1f2a82c682d54527359357cb816ace543b0
JinoRino <adg0609@naver.com>
2021-08-17 17:25:05 +0900

    @Repository 삭제

--- src/main/java/com/jinho/homepage/repository/BoardRepository.java
+++ src/main/java/com/jinho/homepage/repository/BoardRepository.java
@@ -4,7 +4,6 @@ import com.jinho.homepage.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 
 }
