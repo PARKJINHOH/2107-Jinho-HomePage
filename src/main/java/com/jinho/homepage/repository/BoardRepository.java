bfcba311021cfa79ed0c0fa734ff9c293bad70cf
JinoRino <adg0609@naver.com>
2021-10-07 15:13:23 +0900

    git rm -r --cached .

--- src/main/java/com/jinho/homepage/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jinho.homepage.repository;
-
-import com.jinho.homepage.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-
-}
--- src/main/java/com/jinho/homepage/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jinho.homepage.repository;
-
-import com.jinho.homepage.entity.UploadFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ImageRepository extends JpaRepository<UploadFileEntity, Long> {
-}
