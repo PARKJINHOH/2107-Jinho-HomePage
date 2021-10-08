7466fe1f66802eb3feca19239a5dc8fabf6a6804
JinoRino <adg0609@naver.com>
2021-10-08 14:48:15 +0900

    OAuth2 구현

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # 2107_Jinho_HomePage
-
-Maven Project
-Java Version : 11
-Spring Boot version : 2.5.2
-
+* Maven Project
+* Java Version : 11
+* Spring Boot version : 2.5.2
 
 # Stack
 1. Spring Data JPA
 2. MariaDB
 3. Oracle Cloud (Free tier)
+4. Google OAuth2 Login
 
-# 구현 List
+# Todo List
 * [로그인 페이지]
-1. Google OAuth2 구현하기
-   https://console.cloud.google.com/projectselector2/apis/dashboard?pli=1&supportedpurview=project
-2. 
+1. ~~Google OAuth2 구현하기~~
+   * [참고]
+   * bit.ly/3mvjD3p // 구현 방법
+   * bit.ly/3lmK7Vb // Google Cloud Platform
 
-* [application.yml Git Ignore 분리하기]
-1. properties 환경 분리하기 ex) 리눅스 내부 폴더에 있는 파일 읽게 하기.
-이후 properties 파일이 변경되면 해당 디렉토리에 덮어쓰기
+2. [application.yml Git Ignore 분리하기]
+   * properties 환경 분리하기 ex) 리눅스 내부 폴더에 있는 파일 읽게 하기.
+     이후 properties 파일이 변경되면 해당 디렉토리에 덮어쓰기
--- src/main/java/com/jinho/homepage/service/UserService.java
+++ src/main/java/com/jinho/homepage/service/UserService.java
@@ -1,20 +1,34 @@
 package com.jinho.homepage.service;
 
+import com.jinho.homepage.dto.Role;
+import com.jinho.homepage.dto.SessionUser;
 import com.jinho.homepage.dto.UserDto;
+import com.jinho.homepage.entity.OAuthAttributes;
 import com.jinho.homepage.entity.UserEntity;
 import com.jinho.homepage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService implements UserDetailsService, OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
+    private final HttpSession httpSession;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -30,9 +44,38 @@ public class UserService implements UserDetailsService {
         UserEntity user = UserEntity.builder()
                 .email(userDto.getEmail())
                 .nickName(userDto.getNickName())
-                .authority("USER")
+                .role(Role.USER)
                 .password(userDto.getPassword()).build();
 
         return userRepository.save(user).getUserSeq(); // 회원을 저장하고 저장한 회원의 id를 return.
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        UserEntity user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private UserEntity saveOrUpdate(OAuthAttributes attributes) {
+        UserEntity user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getEmail()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
     }
 }
