acf627d7c2da6d3fb50a4ea40dbad459059046ea
JinoRino <adg0609@naver.com>
2021-10-14 12:59:50 +0900

    오타 수정 및 인증 토근 생성 메소드 구현

--- src/main/java/com/jinho/homepage/service/email/ConfirmationTokenService.java
+++ src/main/java/com/jinho/homepage/service/email/ConfirmationTokenService.java
@@ -2,14 +2,16 @@ package com.jinho.homepage.service.email;
 
 import com.jinho.homepage.entity.EmailToken;
 import com.jinho.homepage.exception.BadRequestException;
-import com.jinho.homepage.util.ValidationConstant;
 import com.jinho.homepage.repository.EmailTokenRepository;
+import com.jinho.homepage.util.ValidationConstant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -19,9 +21,9 @@ public class ConfirmationTokenService {
     private final EmailTokenRepository emailTokenRepository;
     private final EmailSenderService emailSenderService;
 
-    public String createEmailConfirmationToken(String email){
+    public String createEmailConfirmationToken(String email) {
 
-        Assert.hasText(email,"receiver Email은 필수 입니다.");
+        Assert.hasText(email, "receiver Email은 필수 입니다.");
 
         EmailToken emailConfirmationToken = EmailToken.createEmailConfirmationToken(email);
         emailTokenRepository.save(emailConfirmationToken);
@@ -30,7 +32,7 @@ public class ConfirmationTokenService {
         mailMessage.setTo(email);
         mailMessage.setSubject("회원가입 이메일 인증");
         // Todo : localhost 변경하기
-        mailMessage.setText("http://localhost:8080/confirm-email?token="+emailConfirmationToken.getId());
+        mailMessage.setText("http://localhost:8080/confirm-email?token=" + emailConfirmationToken.getId());
         emailSenderService.sendEmail(mailMessage);
 
         return emailConfirmationToken.getId();
@@ -38,11 +40,12 @@ public class ConfirmationTokenService {
 
     /**
      * 유효한 토큰 가져오기
+     *
      * @param confirmationTokenId
      * @return
      */
     public EmailToken findByIdAndExpirationDateAfterAndExpired(String confirmationTokenId) {
-        Optional<EmailToken> confirmationToken = emailTokenRepository.findByIdAndExpirationDateAfterAndExpired(confirmationTokenId, LocalDateTime.now(),false);
+        Optional<EmailToken> confirmationToken = emailTokenRepository.findByIdAndExpirationDateAfterAndExpired(confirmationTokenId, LocalDateTime.now(), false);
         EmailToken emailToken = null;
         try {
             emailToken = confirmationToken.orElseThrow(() -> new BadRequestException(ValidationConstant.TOKEN_NOT_FOUND));
@@ -57,6 +60,31 @@ public class ConfirmationTokenService {
      */
     public void updateToken(EmailToken authenticationEmail) {
         emailTokenRepository.save(authenticationEmail);
+    }
+
+    /**
+     * 인증 토큰 생성 메소드
+     */
+    protected String generateAuthToken() {
+        int SIZE = 10;
+        char[] charSet = new char[]{
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '!', '@', '#', '$', '%', '^', '&'};
+
+        StringBuffer sb = new StringBuffer();
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(new Date().getTime());
+
+        int idx = 0;
+        int len = charSet.length;
+        for (int i = 0; i < SIZE; i++) {
+            idx = sr.nextInt(len);
+            sb.append(charSet[idx]);
+        }
+
+        return sb.toString();
     }
 
 }
